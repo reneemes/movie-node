b3deeb2c82848ad81c3363e525f6ddb4a4b45ba4
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2026-01-08 18:45:31 -0500

    Feat: setup for a frontend

--- src/app.js
+++ src/app.js
@@ -1,32 +1,32 @@
 require('dotenv').config();
-// const path = require('path');
+const path = require('path');
 const express = require('express');
-// const hbs = require('hbs');
+const hbs = require('hbs');
 const movieService = require('./utils/movieService');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 // Define paths for Express config
-// const publicDirectoryPath = path.join(__dirname, '../public');
-// const viewsPath = path.join(__dirname, '../templates/views');
-// const partialsPath = path.join(__dirname, '../templates/partials');
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup handlebars engine and view location
-// app.set('view engine', 'hbs');
-// app.set('views', viewsPath);
-// hbs.registerPartials(partialsPath);
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-// app.use(express.static(publicDirectoryPath));
+app.use(express.static(publicDirectoryPath));
 
-// app.get('', (req, res) =>{
-//   res.render('index', {
-//     title: 'Viewing Party',
-//     name: 'Renee Messersmith'
-//   });
+app.get('', (req, res) =>{
+  res.render('index', {
+    title: 'Rancid Tomatillos',
+    name: 'Renee Messersmith'
+  });
   
-// });
+});
 
 app.get('/movies', (req, res) =>{
   const { title } = req.query;
